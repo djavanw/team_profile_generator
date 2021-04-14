2aff5098c88ac60e32645b1a90228cf6add83b9a
Javan <71285290+djavanw@users.noreply.github.com>
2021-04-14 17:15:35 -0600

    Made htmlGenerated a function.

--- index.js
+++ index.js
@@ -10,15 +10,17 @@ const Intern = require("./lib/Intern");
 const OUTPUT_DIR = path.resolve(__dirname, "output");
 const outputPath = path.join(OUTPUT_DIR, "team.html");
 
-const render = require("./src/page-template.js");
+//const render = require("./src/page-template.js");
 
 // const teamMembers = [];
 // const idArray = [];
 
 // function appMenu () {
 
+//}
+function htmlGenerated () {
+  return `<h1> Hello World </h1>`;
 }
-
 
 const questions = [
   {
